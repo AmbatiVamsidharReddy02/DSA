8ff5334f88e01cc8e6ccf46035242f4e3f42e9e2
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-01-18 19:46:05 +0530

    Time: 2 ms (85.06%), Space: 46.3 MB (10.05%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public void moveZeroes(int[] nums) {
+        int lastZeroIndex = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i] = nums[lastZeroIndex];
+                nums[lastZeroIndex] = temp;
+
+                lastZeroIndex++;
+            }
+            
+        }
+
+        
+
+    }
+}
