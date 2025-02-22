4022c9e46430301cc6cff3b4e0682c8bb06c1099
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-02-22 20:22:37 +0530

    Time: 2 ms (94.85%), Space: 46.9 MB (20.36%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
-        // we are going to use the power 2 as the solution 
-        // and we are going to use the significant bit for numbers like
-        // 2 , 4 ,8 , 16, 32 etc...
-
+        if(n==0){
+            return new int[n+1];
+        }
         int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
         int offset = 1;
-        for(int i=1;i<=n;i++){
+        for(int i=2;i<=n;i++){
             if(offset * 2 == i){
                 offset = i;
             }
-            dp[i] = 1 + dp[i-offset];
+            dp[i] = dp[i-offset] + 1;
         }
         return dp;
     }
