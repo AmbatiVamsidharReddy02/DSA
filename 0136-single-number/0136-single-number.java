5da9a5d19395af741648ed9ae0d0465523d1af65
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-02-22 20:00:38 +0530

    Time: 1 ms (99.86%), Space: 46.8 MB (5.67%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,15 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
-        // XOR operation
-        // X ^ 0 = X
-        // X ^ X = 0
-
-        // if we xor all the numbers in the array we get the element
-        // since duplicates cancel out each other.
-        for(int num: nums){
-            result = result ^ num;
+        for(int i=0;i<nums.length;i++){
+            result ^= nums[i];
         }
         return result;
     }
+
 }
