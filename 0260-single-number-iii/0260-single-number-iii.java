69de8958cd9a2e0f0a9c1066265942e45022ff9e
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-02-22 21:09:12 +0530

    Time: 1 ms (100%), Space: 45.9 MB (29.21%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        // do the xor for all the numbers to get the xor for the 
-        // unique elements in the array.
-        int xor = 0;
-        for(int num:nums){
-            xor ^= num;
+        int arrayXor = 0;
+
+        for(int i=0;i<nums.length;i++){
+            arrayXor ^= nums[i];
         }
-
-        // now to segregate the numbers which are different.
-        // we are going to use the set bit from the xor operation before
-        // so, we run thru all the elements in the array, to see 
-        // if the set bit is 0 or 1 in that number then we xor them. 
-
-        // why this works. well the xor operation from before gives the set
-        // bit only when there is a 0 or 1 in both the unique numbers.
-        // so we use this,
-
-        int rightSetBit = xor & -xor;
-        int result[] = new int[2];
-        for(int num:nums){
-            if((rightSetBit & num) == 0){
-                result[0] ^= num;
+        int diff = arrayXor & (-arrayXor);
+        int res1 = 0;
+        int res2 = 0;
+        for(int i=0;i<nums.length;i++){
+            if((nums[i] & diff) == 0){
+                res1 ^= nums[i];
             }else{
-                result[1] ^= num;
+                res2 ^= nums[i];
             }
         }
-        return result;
+        return new int[]{res1, res2};
     }
 }
