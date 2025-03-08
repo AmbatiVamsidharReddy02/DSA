1789d2a422ea780a8bd390e65430ed1a8eaaccfb
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-03-08 23:10:28 +0530

    Time: 0 ms (100%), Space: 42.6 MB (10.83%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,25 +1,29 @@
-public class Solution {
+class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
-        
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int left = 0;
-        int right = m * n - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            // Calculate row and column from the mid index
-            int midElement = matrix[mid / n][mid % n];
-            
-            if (midElement == target) {
-                return true; // Target found
-            } else if (midElement < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int left = 0, right = matrix.length-1; 
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(target>=matrix[mid][0] && target<=matrix[mid][matrix[0].length-1]){
+                // do binary search on this row.
+                int rowLeft = 0, rowRight = matrix[0].length-1;
+                while(rowLeft<=rowRight){
+                    int rowMid = rowLeft + (rowRight-rowLeft)/2;
+                    if(target == matrix[mid][rowMid]){
+                        return true;
+                    }else if(target>matrix[mid][rowMid]){
+                        rowLeft = rowMid + 1;
+                    }else{
+                        rowRight = rowMid -1;
+                    }
+                }
+                return false;
+            }else if(target>matrix[mid][matrix[0].length-1]){
+                left = mid+1;
+            }else{
+                right = mid-1;
             }
         }
-        return false; // Target not found
+        return false;
     }
 }
