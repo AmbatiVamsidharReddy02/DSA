48d2aed2bac020ecf2f35766f980066a8a80239e
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-04-01 21:08:17 +0530

    Time: 6 ms (100%), Space: 44.7 MB (60.24%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,34 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
+public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null){
-            return null;
-        }
-
-        if(root.val == p.val || root.val == q.val){
+        // Base case:
+        if (root == null || root == p || root == q) {
             return root;
         }
 
+        // Perform recursive search in the left subtree
         TreeNode left = lowestCommonAncestor(root.left, p, q);
+
+        // Perform recursive search in the right subtree
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if(left !=null && right!=null){
+        // If both calls returned non-null, current node is LCA
+        if (left != null && right != null) {
             return root;
         }
 
-        if(left!=null){
-            return left;
-        }
-
-            return right;
-        
+        // If one of them is non-null, return it as potential LCA
+        return left != null ? left : right;
     }
 }
