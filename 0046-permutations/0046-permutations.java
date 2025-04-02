eb161e97c748910c12fd755ce865009e3d89bf80
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-04-02 19:29:48 +0530

    Time: 1 ms (92.94%), Space: 44.8 MB (19.6%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,31 @@
-class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
-        return result;
-    }
-
-    private void backtrack(List<List<Integer>> result, List<Integer> currArray, int[] nums, boolean[] used) {
-        if (currArray.size() == nums.length) {
-            result.add(new ArrayList<>(currArray));
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (used[i]) {
-                    continue;
+        
+        // Begin by adding the first number as the first permutation
+        result.add(new ArrayList<>());
+        
+        // Process each number in the input array
+        for (int num : nums) {
+            List<List<Integer>> newPermutations = new ArrayList<>();
+            
+            // Expand each existing permutation
+            for (List<Integer> perm : result) {
+                // Insert current number in every possible position
+                for (int i = 0; i <= perm.size(); i++) {
+                    List<Integer> newPerm = new ArrayList<>(perm);
+                    newPerm.add(i, num); // Insert current number
+                    newPermutations.add(newPerm);
                 }
-
-                used[i] = true;
-                currArray.add(nums[i]);
-                backtrack(result, currArray, nums, used);
-
-                used[i] = false;
-                currArray.remove(currArray.size() - 1);
             }
+            
+            // Update result with new permutations including the current number
+            result = newPermutations;
         }
-
+        
+        return result;
     }
 }
