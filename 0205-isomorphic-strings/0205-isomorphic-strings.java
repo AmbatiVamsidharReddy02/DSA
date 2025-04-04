42f0732044a4b051db7f83476c6f13d6b6b5bc66
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-04-04 19:19:01 +0530

    Time: 35 ms (5.17%), Space: 42.5 MB (70.41%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,34 +1,32 @@
 class Solution {
 
     public boolean isIsomorphic(String s, String t) {
-
-        
-        if (s.length() != t.length()) {
-            return false;
-        }
-        // use two hashmaps to have the mappings between s->t and t->s
-        Map<Character, Character> sToTmap = new HashMap<>();
-        Map<Character, Character> tToSmap = new HashMap<>();
-
+        Map<Character, Character> sTot = new HashMap<>();
+        Map<Character, Character> tTos = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
-
-            // Check mapping from s to t
-            if (sToTmap.containsKey(charS)) {
-                if (sToTmap.get(charS) != charT) return false;
-            } else {
-                sToTmap.put(charS, charT);
+            
+            if(sTot.containsKey(s.charAt(i)) && sTot.get(s.charAt(i))!=t.charAt(i)){
+                return false;
             }
 
-            // Check mapping from t to s
-            if (tToSmap.containsKey(charT)) {
-                if (tToSmap.get(charT) != charS) return false;
-            } else {
-                tToSmap.put(charT, charS);
+             if(tTos.containsKey(t.charAt(i)) && tTos.get(t.charAt(i))!=s.charAt(i)){
+                return false;
             }
+
+            sTot.put(s.charAt(i), t.charAt(i));
+            tTos.put(t.charAt(i), s.charAt(i));
+    //        String mapAsString = sTot.keySet().stream()
+    //   .map(key -> key + "=" + sTot.get(key))
+    //   .collect(Collectors.joining(", ", "{", "}"));
+
+    //   String mapAsString2 = tTos.keySet().stream()
+    //   .map(key -> key + "=" + tTos.get(key))
+    //   .collect(Collectors.joining(", ", "{", "}"));
+    //   System.out.println(mapAsString);
+    //   System.out.println(mapAsString2);
         }
+
         return true;
     }
 }
