5831eeacdfb9f3db74c92bcf143cefb6a9938aa5
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-04-04 21:13:31 +0530

    Time: 10 ms (26.76%), Space: 49.5 MB (43.27%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -7,6 +7,7 @@ class NumArray {
         for(int i=1;i<=nums.length;i++){
             prefix[i] = prefix[i-1] + nums[i-1];
         }
+        System.out.println(Arrays.toString(prefix));
     }
     
     public int sumRange(int left, int right) {
