ddff9f01c082ce9a91bd8c49fbbb9bc0cb2898a3
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-13 21:10:08 +0530

    Time: 22 ms (3.82%), Space: 47.5 MB (8.96%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,67 +1,70 @@
 class Solution {
+    public void setZeroes(int[][] matrix) {
+        // first check the first row and first column to see if there are any zeroes in
+        // them to prevent overriding.
 
-    public void setZeroes(int[][] a) {
-        boolean rowZero = false, columnZero = false;
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
-                if (a[i][j] == 0) {
-                    if (i == 0) {
-                        rowZero = true;
-                    }
-                    if (j == 0) {
-                        columnZero = true;
-                    }
-                    a[i][0] = 0;
-                    a[0][j] = 0;
-                }
-            }
-        }
-        printArray(a);
-        System.out.println(a.length);
-        System.out.println(a[0].length);
-        for (int i = 1; i < a.length; i++) {
-            if (a[i][0] == 0) {
-                for (int j = 1; j < a[0].length; j++) {
-                    a[i][j] = 0;
-                }
+        boolean rowZero = false;
+        boolean columnZero = false;
+        int rowLength = matrix.length;
+        int columnLength = matrix[0].length;
+        for (int i = 0; i < rowLength; i++) {
+            if (matrix[i][0] == 0) {
+                columnZero = true;
+                break;
             }
         }
 
-        printArray(a);
+        for (int i = 0; i < columnLength; i++) {
+            if (matrix[0][i] == 0) {
+                rowZero = true;
+                break;
+            }
+        }
 
-        for (int i = 1; i < a[0].length; i++) {
-            if (a[0][i] == 0) {
-                for (int j = 1; j < a.length; j++) {
-                    a[j][i] = 0;
+        // loop thru the inner array and if we find the zero element mark the first row
+        // and first column to 0.
+        for (int i = 1; i < rowLength; i++) {
+            for (int j = 1; j < columnLength; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        printArray(a);
+        System.out.println(Arrays.deepToString(matrix));
+        // now paint the matrix with 0s based on row zero and column zero.
 
-        if (columnZero) {
-            for (int i = 0; i < a.length; i++) {
-                a[i][0] = 0;
+        for (int i = 1; i < rowLength; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < columnLength; j++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        printArray(a);
-        if (rowZero) {
-            for (int i = 1; i < a[0].length; i++) {
-                a[0][i] = 0;
-            }
+        System.out.println(Arrays.deepToString(matrix));
 
-            printArray(a);
+        for (int i = 1; i < columnLength; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 1; j < rowLength; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
         }
-        printArray(a);
+        System.out.println(Arrays.deepToString(matrix));
+        System.out.println(rowZero + " "+ columnZero);
+        if(rowZero){
+            for(int i=0;i<columnLength;i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if(columnZero){
+            for(int i=0;i<rowLength;i++){
+                matrix[i][0] = 0;
+            }
+        }
+        System.out.println(Arrays.deepToString(matrix));
+
     }
 
-    public void printArray(int[][] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
 }
