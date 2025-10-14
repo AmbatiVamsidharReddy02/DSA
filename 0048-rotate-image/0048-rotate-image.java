2ac7b5ac432180049fd7072986d7564e583e9c7d
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-14 23:20:24 +0530

    Time: 0 ms (100%), Space: 42.3 MB (61.17%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,29 +1,33 @@
 class Solution {
     public void rotate(int[][] matrix) {
-    int n = matrix.length;
-    
-    // Transpose the matrix
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            // Swap elements on the diagonal
-            int temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
+        // rotate the array.
+        // first vertically reverse the array meaning swap elements in each columns top
+        // becomes bottom, second top becomes second bottom and so on..
+
+        int len = matrix.length;
+        // lets start with row 0 and iterate till len/2 (only half)
+
+        int row = 0;
+        while (row < len / 2) {
+            // now for each row traverse columns and swap them one by one of that row.
+            for (int i = 0; i < len; i++) {
+                int temp = matrix[row][i];
+                matrix[row][i] = matrix[len - row - 1][i];
+                matrix[len - row - 1][i] = temp;
+            }
+            row++;
+        }
+
+        // vertical swapping is done.
+        // now for transpose where we swap rows to columns and columns to rows
+        // (basically swapping all the opposite elements wrt diagonal of a matrix)
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
         }
     }
-    
-    // Reverse each row
-    for (int i = 0; i < n; i++) {
-        int start = 0;
-        int end = n - 1;
-        while (start < end) {
-            // Swap elements to reverse the row
-            int temp = matrix[i][start];
-            matrix[i][start] = matrix[i][end];
-            matrix[i][end] = temp;
-            start++;
-            end--;
-        }
-    }
-}
 }
