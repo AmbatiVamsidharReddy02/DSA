0db105bdb209718989be3faefb113d22a96a6fbd
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-14 22:32:50 +0530

    Time: 0 ms (100%), Space: 42 MB (68.12%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,33 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // using extra space with o(n) time.
-        int zeroes = 0;
-        int ones = 0;
-        int twos = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                zeroes++;
-            if (nums[i] == 1)
-                ones++;
-            if (nums[i] == 2)
-                twos++;
-        }
+        // using dutch national flag algorithm
 
-        for (int i = 0; i < zeroes; i++) {
-            nums[i] = 0;
+        // take three pointers low, mid, high
+        // move mid as the traversal pointer till mid<=high
+        // scenarios -
+        // if mid == 0 then swap the mid, low element and increment both.
+        // if mid == 1 then continue forward.
+        // if the mid == 2 then swap the high, mid elements and then decrement only high
+        // value.
+
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums, mid, high);
+                high--;
+            } else {
+                mid++;
+            }
         }
-        for (int i = 0; i < ones; i++) {
-            nums[i + zeroes] = 1;
-        }
-        for (int i = 0; i < twos; i++) {
-            nums[i + zeroes + ones] = 2;
-        }
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
