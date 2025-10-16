31755952973434e47bb6d4d4f200de607082bced
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-16 23:33:33 +0530

    Time: 1 ms (99.39%), Space: 57.1 MB (57.32%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // if the current sum is less than the currEle restart the subarray from the
-        // currEle.
-        // compare max with (max, currSum, CurrEle) use greater value as max
-        int max = Integer.MIN_VALUE;
-        int currSubSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (currSubSum + nums[i] >= nums[i]) {
-                currSubSum = currSubSum + nums[i];
-            } else {
-                currSubSum = nums[i];
+        int currSum = 0;
+        int max = nums[0];
+        for( int i=0;i<nums.length;i++){
+            if(currSum<0){
+                currSum = 0;
             }
-            max = Math.max(max, Math.max(nums[i], currSubSum));
+            currSum = currSum + nums[i];
+            max = Math.max(max, Math.max(currSum, nums[i]));
         }
         return max;
     }
