aff352f9a253a800eb06b60d4cd359209b1aec9c
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-17 21:51:12 +0530

    Time: 2 ms (16.9%), Space: 45.6 MB (85.17%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,30 +1,23 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        // first check the first row and first column to see if there are any zeroes in
-        // them to prevent overriding.
-
-        boolean rowZero = false;
-        boolean columnZero = false;
-        int rowLength = matrix.length;
-        int columnLength = matrix[0].length;
-        for (int i = 0; i < rowLength; i++) {
+        // find if the 1st row and 1st column has any zeroes
+        boolean rowZero = false, columnZero = false;
+        for (int i = 0; i < matrix.length; i++) {
             if (matrix[i][0] == 0) {
+                rowZero = true;
+                break;
+            }
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
                 columnZero = true;
                 break;
             }
         }
 
-        for (int i = 0; i < columnLength; i++) {
-            if (matrix[0][i] == 0) {
-                rowZero = true;
-                break;
-            }
-        }
-
-        // loop thru the inner array and if we find the zero element mark the first row
-        // and first column to 0.
-        for (int i = 1; i < rowLength; i++) {
-            for (int j = 1; j < columnLength; j++) {
+        // now traverse the entire array from 1
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
@@ -32,39 +25,33 @@ class Solution {
             }
         }
 
-        System.out.println(Arrays.deepToString(matrix));
-        // now paint the matrix with 0s based on row zero and column zero.
-
-        for (int i = 1; i < rowLength; i++) {
+        // now update the array to zeroes. 
+        for (int i = 1; i < matrix.length; i++) {
             if (matrix[i][0] == 0) {
-                for (int j = 1; j < columnLength; j++) {
+                for (int j = 1; j < matrix[0].length; j++) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        System.out.println(Arrays.deepToString(matrix));
 
-        for (int i = 1; i < columnLength; i++) {
-            if (matrix[0][i] == 0) {
-                for (int j = 1; j < rowLength; j++) {
-                    matrix[j][i] = 0;
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < matrix.length; i++) {
+                    matrix[i][j] = 0;
                 }
             }
         }
-        System.out.println(Arrays.deepToString(matrix));
-        System.out.println(rowZero + " "+ columnZero);
-        if(rowZero){
-            for(int i=0;i<columnLength;i++){
-                matrix[0][i] = 0;
-            }
-        }
-        if(columnZero){
-            for(int i=0;i<rowLength;i++){
+
+        // fix the 1st row and column based on flags
+        if (rowZero) {
+            for (int i = 0; i < matrix.length; i++) {
                 matrix[i][0] = 0;
             }
         }
-        System.out.println(Arrays.deepToString(matrix));
-
+        if (columnZero) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
     }
-
 }
