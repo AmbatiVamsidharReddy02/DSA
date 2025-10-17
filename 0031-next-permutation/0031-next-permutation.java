00bdc2969872a2d6e89d68484adc8c1b6c8071cb
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-17 22:27:57 +0530

    Time: 3 ms (3.77%), Space: 43.3 MB (38.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,44 +1,37 @@
 class Solution {
-    public static void reverseRange(int[] arr, int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start++] = arr[end];
-            arr[end--] = temp;
-        }
-    }
-
     public void nextPermutation(int[] nums) {
+        // find any dip
         // 2 1 5 4 3 0 0
-
         int index = -1;
-        int n = nums.length;
-        // find the dip
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                index = i - 1;
                 break;
             }
         }
+        System.out.println(index);
         if (index == -1) {
-            // reverse the array and return it as result;
-            reverseRange(nums, 0, n-1);
+            Arrays.sort(nums);
         } else {
-            // found the dip
-            // check for the greatest minimum from the prefix.
-            int currEle = nums[index];
-            for (int i = n - 1; i >= 0; i--) {
-                if (nums[i] > currEle) {
-                    // swap them
-                    currEle = nums[i];
-                    nums[i] = nums[index];
-                    nums[index] = currEle;
+            // swap the index elment with the first highest number found from the end.
+            for (int i = nums.length - 1; i > index; i--) {
+                if (nums[index] < nums[i]) {
+                    int temp = nums[index];
+                    nums[index] = nums[i];
+                    nums[i] = temp;
                     break;
                 }
             }
-
-            // now reverse the entire prefix from index+1 to end of array, to get the next
-            // permutation.
-            reverseRange(nums, index + 1, n-1);
+            // reverse the array from index + 1 to end of array to get the next permuation
+            int left = index + 1, right = nums.length - 1;
+            while (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
         }
+
     }
 }
