7cd2d9124a6dcdfa331d7ffae5e40670f40a90ab
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-17 22:06:20 +0530

    Time: 7 ms (1.14%), Space: 42.4 MB (8.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,20 +1,24 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        // if numRows = 1
         List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>());
-        res.get(0).add(1);
-        if (numRows == 1)
-            return res;
-        for (int i = 1; i < numRows; i++) {
-            // add leaf node
-            res.add(new ArrayList<>());
-            res.get(i).add(1);
-            for (int j = 0; j <= (i - 2); j++) {
-                res.get(i).add(res.get(i - 1).get(j) + res.get(i - 1).get(j + 1));
+
+        if (numRows >= 1) {
+            List<Integer> currList = new ArrayList<>();
+            currList.add(1);
+            res.add(currList);
+        }
+
+        for (int i = 2; i <= numRows; i++) {
+            // add leaf
+            List<Integer> currList = new ArrayList<>();
+            currList.add(1);
+            // loop thru to find the calcuation nodes
+            for (int j = 0; j < i - 2; j++) {
+                currList.add(res.get(i - 2).get(j) + res.get(i - 2).get(j + 1));
             }
-            // another leaf
-            res.get(i).add(1);
+            currList.add(1);
+            System.out.println(currList);
+            res.add(currList);
         }
         return res;
     }
