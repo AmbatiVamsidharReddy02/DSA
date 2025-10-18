6d104040ce5c0f8fd0ea99f09fbfbc15f4b10529
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-18 20:31:09 +0530

    Time: 4 ms (90.48%), Space: 59 MB (10.23%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,22 +1,21 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // use two pointer slow and fast to detect the cycle once cycle is found
-        // take another pointer slow1 or(use fast as another slow pointer) to nums[0] and then loop till slow and slow1 meet. return that element
-        int fast = nums[0];
-        int slow = nums[0];
+        // use two pointer fast and slow to detect a cycle since the numbers in array are 
+        // 1,n inclusive and having a duplicate meaning there will be a cycle. and all the
+        // array elements are within the boundaries of the array length. 
 
+        int slow = nums[0];
+        int fast = nums[0];
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
         }while(slow!=fast);
-
-        // found the cycle
+        
         fast = nums[0];
-        while(slow!=fast){
+        while(fast!=slow){
             fast = nums[fast];
             slow = nums[slow];
         }
-        return slow;
-
+        return fast;
     }
 }
