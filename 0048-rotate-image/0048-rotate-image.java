d094c05a523303d3dfdd3cae2a958cfca52c726d
AmbatiVamsidharReddy02 <109794951+AmbatiVamsidharReddy02@users.noreply.github.com>
2025-10-18 19:46:46 +0530

    Time: 3 ms (2.24%), Space: 42.9 MB (7.64%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,33 +1,25 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        // rotate the array.
-        // first vertically reverse the array meaning swap elements in each columns top
-        // becomes bottom, second top becomes second bottom and so on..
-
-        int len = matrix.length;
-        // lets start with row 0 and iterate till len/2 (only half)
-
-        int row = 0;
-        while (row < len / 2) {
-            // now for each row traverse columns and swap them one by one of that row.
-            for (int i = 0; i < len; i++) {
-                int temp = matrix[row][i];
-                matrix[row][i] = matrix[len - row - 1][i];
-                matrix[len - row - 1][i] = temp;
+        // to rotate a matrix. 
+        // 1. reverse numbers every column one by one.
+        for( int i=0;i<matrix[0].length;i++) {
+            int low = 0, high = matrix.length-1;
+            while (low <= high) {
+                int temp = matrix[low][i];
+                matrix[low][i] = matrix[high][i];
+                matrix[high][i] = temp;
+                low++;high--;
             }
-            row++;
         }
-
-        // vertical swapping is done.
-        // now for transpose where we swap rows to columns and columns to rows
-        // (basically swapping all the opposite elements wrt diagonal of a matrix)
-
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
+        System.out.println(Arrays.deepToString(matrix));
+        // 2. transpose the matrix
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i+1;j< matrix.length;j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
